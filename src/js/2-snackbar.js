33ab21bdc4037573cfee13595ac649a491e990e9
liudmyla21-belikova <liudmyla.belikova@gmail.com>
2025-06-06 01:00:28 +0300

    add js and css

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const { delay, state } = event.target.elements;
+
+  const delayValue = +delay.value;
+  const stateValue = state.value;
+
+  setTimeout(() => {
+    return new Promise((resolve, reject) => {
+      if (stateValue === 'fulfilled') {
+        resolve();
+      } else {
+        reject();
+      }
+    })
+      .then(() =>
+        iziToast.success({
+          message: `✅ Fulfilled promise in ${delayValue}ms`,
+          closeOnClick: true,
+          position: 'topRight',
+        })
+      )
+      .catch(() =>
+        iziToast.error({
+          message: `❌ Rejected promise in ${delayValue}ms`,
+          closeOnClick: true,
+          position: 'topRight',
+        })
+      );
+  }, delayValue);
+}
